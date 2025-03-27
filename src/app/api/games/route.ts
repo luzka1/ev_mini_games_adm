0d62669112aa796fd214ee92a40c06aff2226161
lucas <lucasgsantos1727@gmail.com>
2025-03-27 17:52:16 -0300

    refactor: fix de erro de requisição

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import Parse from "@/lib/parse";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const result = await Parse.Cloud.run("getGames");
 
